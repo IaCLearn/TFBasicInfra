753e9bfcffdb6e4d168596080e2d41fe94e1b174
shola lawani <sholalawani@microsoft.com>
2023-06-15 04:02:52 -0400

    redish cache

--- modules/rediscache/rediscache.tf
+++ modules/rediscache/rediscache.tf
@@ -2,8 +2,8 @@ data "azurerm_resource_group" "keyvault" {
   name = "${var.existingrgname}-rg"
 }
 # NOTE: the Name used for Redis needs to be globally unique
-resource "azurerm_redis_cache" "example" {
-  name                = "example-cache"
+resource "azurerm_redis_cache" "azredcache" {
+  name                = "errovam001"
  location                    = data.azurerm_resource_group.keyvault.location
   resource_group_name         = data.azurerm_resource_group.keyvault.name
   capacity            = var.capacity

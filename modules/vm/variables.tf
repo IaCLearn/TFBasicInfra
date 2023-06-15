452ea9c210dfe0969dc73c12495d25bac9832f77
shola lawani <sholalawani@microsoft.com>
2023-06-15 05:21:33 -0400

    added vm module with correct spell casing

---
+++ modules/vm/variables.tf
@@ -0,0 +1,122 @@
+variable "existingdbsnetid" {
+type=string
+description="Existing db Subnet ID"
+
+}
+
+variable "existingappsnetid" {
+type=string
+description="Existing db Subnet ID"
+
+}
+
+variable "omsapprg_name" {
+    type=string
+    description="The name of the resource for the OMS application"
+}
+
+variable "environment" {
+  type        = string
+  description = "Environment"
+}
+
+variable "location" {
+  default = "canadacentral"
+}
+
+variable "appvmcount" {
+  type = number
+  description = "number of vms to be created"
+}
+
+variable "appvm_names" {
+  type    = string
+
+}
+
+variable "sql_vmname" {
+    type= string
+  description = "name of the SQL Virtual Machine."
+}
+
+variable "vm_dompassword" {
+  description = "Admin Password for the VM's"
+  sensitive   = true
+}
+
+variable "vm_size_sql" {
+  description = "VM Size SQL Server"
+}
+
+variable "sql_vmusername"{
+
+description= "SQL VM user name"
+
+}
+
+variable "vm_os_disk_delete_flag"{
+  description = "Should the OS Disk (either the Managed Disk / VHD Blob) be deleted when the Virtual Machine is destroyed?"
+  default     = "true"
+}
+variable "vm_data_disk_delete_flag"{
+  description = "Should the Data Disks (either the Managed Disks / VHD Blobs) be deleted when the Virtual Machine is destroyed?"
+  default     = "true"
+}
+
+variable "sqladminpwd"{
+  description = "SQL admin password"
+  sensitive   = true
+}
+variable "sqladmin"{
+  description = "SQL admin username"
+  sensitive   = true
+}
+variable "sqldatafilepath"{
+  description = "SQL data file path"
+}
+variable "sqllogfilepath"{
+
+   type=string
+  description = "SQL log file path"
+}
+variable "publisher_sql"{
+  description = "image publisher"
+}
+variable "offer_sql" {
+   type=string
+  description = "image offer"
+}
+variable "sku_sql"{
+   type=string
+  description = "sku of the image"
+}
+variable "image_version_sql" {
+  type=string
+  description = "version for the image"
+}
+
+variable "publisher_windows"{
+  
+  type = string
+
+  description = "version of windows"
+}
+
+variable "offer_windows" {
+
+  type = string
+  description = "offering version of windows"
+}
+    
+variable "sku_windows" {
+  type = string
+  description="Windows version sku"
+}
+variable "version_windows" {
+
+  type = string
+
+  description = "version of windows like latest"
+  
+}
+ 
---
+++ modules/vm/vm.tf
@@ -0,0 +1,261 @@
+resource "azurerm_resource_group" "vm_resource_group" {
+  name     = "${var.omsapprg_name}-rg"
+  location = var.location
+  
+  tags = {
+    Environment = var.environment
+  }
+}
+
+#sql vm creation single machine
+resource "azurerm_network_interface" "db-nic" {
+  name                = "${var.sql_vmname}-db-nic"
+  location            = azurerm_resource_group.vm_resource_group.location
+  resource_group_name = azurerm_resource_group.vm_resource_group.name
+
+  ip_configuration {
+    name                          = "${var.sql_vmname}-IP"
+    subnet_id     =var.existingdbsnetid
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_virtual_machine" "sqlvm" {
+  name                = var.sql_vmname
+  resource_group_name = azurerm_resource_group.vm_resource_group.name
+  location            =azurerm_resource_group.vm_resource_group.location
+  vm_size                = var.vm_size_sql
+  network_interface_ids = [
+    azurerm_network_interface.db-nic.id
+  ]
+
+
+  storage_image_reference {
+    publisher = var.publisher_sql
+    offer     = var.offer_sql
+    sku       = var.sku_sql
+    version   = var.image_version_sql
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  storage_os_disk {
+    name              = "${var.sql_vmname}-OS"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Premium_LRS"
+    disk_size_gb      = 128
+  }
+
+  storage_data_disk {
+    name              = "${var.sql_vmname}-DATA"
+    caching           = "ReadOnly"
+    create_option     = "Empty"
+    disk_size_gb      = 256
+    lun               = 0
+    managed_disk_type = "Premium_LRS"
+  }
+
+  storage_data_disk {
+    name              = "${var.sql_vmname}-LOG"
+    caching           = "ReadOnly"
+    create_option     = "Empty"
+    disk_size_gb      = 128
+    lun               = 1
+    managed_disk_type = "Premium_LRS"
+  }
+
+  os_profile {
+    admin_password = var.sqladminpwd
+    admin_username = var.sql_vmusername
+    computer_name  = var.sql_vmname
+  }
+
+  os_profile_windows_config {
+    timezone           = "UTC"
+    provision_vm_agent = true
+  }
+
+  
+}
+
+#disable domain firewall for domain activities
+
+resource "azurerm_virtual_machine_extension" "vm_extension_modify_fw" {
+   
+    name = "vm_modify_fw"
+    virtual_machine_id = azurerm_virtual_machine.sqlvm.id
+    publisher = "Microsoft.Compute"
+    type = "CustomScriptExtension"
+    type_handler_version = "1.8"
+    auto_upgrade_minor_version = true
+    settings = <<SETTINGS
+    {
+    "commandToExecute": "powershell Set-NetFirewallProfile -Profile Domain -Enabled False"
+    }
+    SETTINGS
+
+     depends_on = [
+     azurerm_virtual_machine.sqlvm, azurerm_mssql_virtual_machine.azurerm_sqlvmmanagement
+    ]
+}
+
+#join sqlvm to domain
+
+resource "azurerm_virtual_machine_extension" "sqldomjoin" {
+ 
+name = "domjoin"
+virtual_machine_id = azurerm_virtual_machine.sqlvm.id
+publisher = "Microsoft.Compute"
+type = "JsonADDomainExtension"
+type_handler_version = "1.3"
+
+settings = <<SETTINGS
+{
+"Name": "phebsix.com",
+"User": "phebsix.com\\localadmin",
+"Restart": "true",
+"Options": "3"
+}
+SETTINGS
+protected_settings = <<PROTECTED_SETTINGS
+{
+"Password": "${var.vm_dompassword}"
+}
+PROTECTED_SETTINGS
+ depends_on = [azurerm_virtual_machine.sqlvm,azurerm_mssql_virtual_machine.azurerm_sqlvmmanagement,azurerm_virtual_machine_extension.vm_extension_modify_fw]
+}
+
+#SQL Management for Azure SQL 
+resource "azurerm_mssql_virtual_machine" "azurerm_sqlvmmanagement" {
+
+  virtual_machine_id               = azurerm_virtual_machine.sqlvm.id
+  sql_license_type                 = "PAYG"
+  sql_connectivity_port            = 1433
+  sql_connectivity_type            = "PRIVATE"
+  sql_connectivity_update_password = var.sqladminpwd
+  sql_connectivity_update_username = var.sqladmin
+
+  auto_patching {
+    day_of_week                            = "Sunday"
+    maintenance_window_duration_in_minutes = 60
+    maintenance_window_starting_hour       = 2
+  }
+
+  storage_configuration {
+    disk_type             = "NEW"  # (Required) The type of disk configuration to apply to the SQL Server. Valid values include NEW, EXTEND, or ADD.
+    storage_workload_type = "OLTP" # (Required) The type of storage workload. Valid values include GENERAL, OLTP, or DW.
+
+    # The storage_settings block supports the following:
+    data_settings {
+      default_file_path = var.sqldatafilepath # (Required) The SQL Server default path
+      luns              = [0]                  #azurerm_virtual_machine_data_disk_attachment.datadisk_attach[count.index].lun]
+    }
+
+    log_settings {
+      default_file_path = var.sqllogfilepath # (Required) The SQL Server default path
+      luns              = [1]                 #azurerm_virtual_machine_data_disk_attachment.logdisk_attach[count.index].lun] # (Required) A list of Logical Unit Numbers for the disks.
+    }
+
+  }
+
+}
+
+#Windows Server VM
+
+
+# Define the virtual machines
+resource "azurerm_network_interface" "app-nic" {
+  count               = "${var.appvmcount}"
+  name                = "${var.appvm_names}${count.index}-nic"
+  resource_group_name = azurerm_resource_group.vm_resource_group.name
+  location            =azurerm_resource_group.vm_resource_group.location
+
+  ip_configuration {
+    name                          = "${var.appvm_names}${count.index}-ipconfig"
+      subnet_id                     = var.existingappsnetid
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_virtual_machine" "winvm" {
+  count                = "${var.appvmcount}"
+  name                 = "${var.appvm_names}${count.index}"
+   resource_group_name = azurerm_resource_group.vm_resource_group.name
+  location            =azurerm_resource_group.vm_resource_group.location
+  network_interface_ids = [azurerm_network_interface.app-nic[count.index].id]
+  vm_size              = "Standard_DS2_v2"
+
+  storage_image_reference {
+    publisher = var.publisher_windows
+    offer     = var.offer_windows
+    sku       = var.sku_windows
+    version   = var.version_windows
+  }
+
+  storage_os_disk {
+    name              = "${var.appvm_names}${count.index}-osdisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  os_profile {
+    computer_name  = "${var.appvm_names}${count.index}"
+    admin_username = var.sql_vmusername
+    admin_password = var.sqladminpwd
+  }
+ os_profile_windows_config {
+    timezone           = "UTC"
+    provision_vm_agent = true
+  }
+   depends_on = [
+     azurerm_virtual_machine.sqlvm, azurerm_mssql_virtual_machine.azurerm_sqlvmmanagement, azurerm_virtual_machine_extension.sqldomjoin
+    ]
+}
+
+
+resource "azurerm_virtual_machine_extension" "winvm_extension_modify_fw" {
+    count= "${var.appvmcount}"
+    name = "vm_modify_fw"
+    virtual_machine_id = azurerm_virtual_machine.winvm[count.index].id
+    publisher = "Microsoft.Compute"
+    type = "CustomScriptExtension"
+    type_handler_version = "1.8"
+    auto_upgrade_minor_version = true
+    settings = <<SETTINGS
+    {
+    "commandToExecute": "powershell Set-NetFirewallProfile -Profile Domain -Enabled False"
+    }
+    SETTINGS
+
+     depends_on = [
+   azurerm_virtual_machine.winvm
+    ]
+}
+
+resource "azurerm_virtual_machine_extension" "windomjoin" {
+ count = "${var.appvmcount}"
+name = "domjoin"
+virtual_machine_id =  azurerm_virtual_machine.winvm[count.index].id
+publisher = "Microsoft.Compute"
+type = "JsonADDomainExtension"
+type_handler_version = "1.3"
+
+settings = <<SETTINGS
+{
+"Name": "phebsix.com",
+"User": "phebsix.com\\localadmin",
+"Restart": "true",
+"Options": "3"
+}
+SETTINGS
+protected_settings = <<PROTECTED_SETTINGS
+{
+"Password": "${var.vm_dompassword}"
+}
+PROTECTED_SETTINGS
+ depends_on = [azurerm_virtual_machine.winvm,azurerm_virtual_machine_extension.winvm_extension_modify_fw]
+}
